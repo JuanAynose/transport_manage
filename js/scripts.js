36f300583b33d18296da7eebb5b5af9dbede2479
NicoCan0 <112355360+NicoCan0@users.noreply.github.com>
2022-09-05 16:40:24 -0300

    modal delete

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,32 @@
 /* cards */
 import initializeMenuListener from './modals/reset/initializeMenuListener.js';
-
+/*empleados container*/
+const editarEmpleado = document.getElementById('editarEmpleado');
+const modalEditButtons = document.getElementById('modalEditButtons');
+const modalEdit = document.getElementById('modalEdit');
 /**/
 window.addEventListener('load', e => {
 	initializeMenuListener();
 });
+
+editarEmpleado.addEventListener("click",ev=>{
+	if(ev.target.textContent === 'Editar'){
+		let getIdEmploye = Number(ev.target.parentElement.previousElementSibling.children[0].children[1].textContent);
+		console.log("edit",getIdEmploye)
+	}else if(ev.target.textContent === 'Borrar'){
+		let getIdEmploye = Number(ev.target.parentElement.previousElementSibling.children[0].children[1].textContent);
+		console.log("delete",getIdEmploye)
+		console.log(modalEdit.children)
+		modalEdit.classList.remove('hidden');
+	}
+})
+
+modalEditButtons.addEventListener("click", ev=>{
+	if(ev.target.textContent === 'Si'){
+		console.log("cagaste")
+		modalEdit.classList.add('hidden');
+	}else{
+		console.log(":D");
+		modalEdit.classList.add('hidden');
+	}
+})

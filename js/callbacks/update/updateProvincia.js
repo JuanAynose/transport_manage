33493559fe11b32dec8f2e84ecf9c7576364b7cf
JuanAynose <88510619+JuanAynose@users.noreply.github.com>
2022-09-30 00:42:56 -0300

    ciudades & provincias crud added

---
+++ js/callbacks/update/updateProvincia.js
@@ -0,0 +1,30 @@
+export const updateProvincia = data => {
+	const { nombre_provincia, id_provincia } = data;
+
+	const sendShit = () => {
+		const myHeaders = new Headers();
+		myHeaders.append('Content-Type', 'application/json');
+
+		const raw = JSON.stringify({
+			nombre_provincia,
+			id_provincia
+		});
+
+		const requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow'
+		};
+
+		fetch(
+			'http://localhost/transport_manage/api/calls/ciudades/update_provincia.php',
+			requestOptions
+		)
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log('error', error));
+	};
+
+	sendShit();
+};

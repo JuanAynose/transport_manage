ea0d501aed9245725053538abf585ef657e0a9cb
JuanAynose <88510619+JuanAynose@users.noreply.github.com>
2022-10-02 20:34:27 -0300

    package changed

---
+++ js/callbacks/get/getSinglePaqueteByDetail.js
@@ -0,0 +1,13 @@
+const getSinglePaqueteByDetail = id => {
+	return fetch(
+		`http://localhost/transport_manage/api/calls/paquetes/leer_paquete_solo_detalle.php?id_paquete=${id}`
+	)
+		.then(res => (res.ok ? Promise.resolve(res) : Promise.reject(res)))
+		.then(res => res.json())
+		.then(res => {
+			return res;
+		})
+		.catch(console.log);
+};
+
+export default getSinglePaqueteByDetail;

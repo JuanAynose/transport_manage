5613ab2a3ddc76e866419ea69138695fec087c6c
JuanAynose <88510619+JuanAynose@users.noreply.github.com>
2022-10-07 10:48:29 -0300

    remitos added

--- js/cardsHandlers/CardRemitoManager.js
+++ js/cardsHandlers/CardRemitoManager.js
@@ -1,9 +1,9 @@
+import CardRemito from '../cards/CardRemito.js';
+
 const CardRemitoManager = (data, containerPaquete) => {
 	if (!data.length) return data;
-	let cont = 0;
 	for (const remitoData of data) {
-		//containerPaquete.innerHTML += CardPackage(packageData, cont);
-		cont += 1;
+		containerPaquete.innerHTML += CardRemito(remitoData);
 	}
 };
 

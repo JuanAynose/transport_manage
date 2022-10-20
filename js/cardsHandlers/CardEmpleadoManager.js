697b8cf57bea31c25a0ef9b847028827655cf039
JuanAynose <88510619+JuanAynose@users.noreply.github.com>
2022-10-20 20:34:28 -0300

    a

--- js/cardsHandlers/CardEmpleadoManager.js
+++ js/cardsHandlers/CardEmpleadoManager.js
@@ -1,8 +1,8 @@
 import CardEmploye from '../cards/CardEmploye.js';
 
 const CardEmpleadoManager = (data, containerEmpleado) => {
-	if(!data.length) return data;
-
+	if (!data.length) return data;
+	console.log(containerEmpleado);
 	containerEmpleado.innerHTML = '';
 	for (const employeData of data) {
 		containerEmpleado.innerHTML += CardEmploye(employeData);

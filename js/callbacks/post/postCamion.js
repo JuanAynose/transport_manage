b1da7e68d1c7adbf1ceb981d560611096da0aea3
JuanAynose <88510619+JuanAynose@users.noreply.github.com>
2022-10-24 19:56:04 -0300

    Update postCamion.js

--- js/callbacks/post/postCamion.js
+++ js/callbacks/post/postCamion.js
@@ -1,3 +1,7 @@
+import { MODAL_OPTIONS } from '../../constants/modalOptions.js';
+import makeCall from '../../modals/controller/makeCall.js';
+const editarCamion = document.getElementById('editarCamion');
+
 export const postCamion = data => {
 	const { capacidad_camion, marca_camion, disponibilidad_camion } = data;
 
@@ -23,7 +27,11 @@ export const postCamion = data => {
 			requestOptions
 		)
 			.then(response => response.text())
-			.then(result => console.log(result))
+			.then(result => {
+				editarCamion.innerHTML = '';
+				makeCall(MODAL_OPTIONS.TRANSPORTE);
+				console.log(result);
+			})
 			.catch(error => console.log('error', error));
 	};
 

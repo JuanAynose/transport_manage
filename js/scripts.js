cfa69d05eba58ec2460bb72027430d7b08652ff1
JuanAynose <88510619+JuanAynose@users.noreply.github.com>
2022-10-26 12:34:56 -0300

    color theme fixed

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,10 @@ const contentSalaryAccordion = document.getElementById(
 	'formSalaryContent__container'
 );
 
+/*theme container*/
+const themeMenu = document.getElementById('theme_color');
+const themeContainer = document.getElementById('theme_container');
 /**/
-
 window.addEventListener('load', e => {
 	initializeMenuListener();
 });
@@ -22,5 +24,41 @@ bttnSalaryAccordion.addEventListener('click', () => {
 	} else {
 		contentSalaryAccordion.classList.add('active');
 		bttnSalaryAccordion.textContent = 'Ver menos';
+	}
+});
+
+/*theme picker color*/
+themeMenu.addEventListener('click', ev => {
+	const getColorName = ev.target.innerText;
+	if (ev.target.textContent === 'Temas') {
+		themeContainer.classList.toggle('active');
+	}
+
+	const clearBodyClass = () => {
+		body.classList.remove(...body.classList);
+		themeContainer.classList.toggle('active');
+	};
+
+	switch (getColorName) {
+		case 'Default':
+			clearBodyClass();
+			body.classList.add('default');
+			break;
+		case 'Rosa':
+			clearBodyClass();
+			body.classList.add('pink');
+			break;
+		case 'Naranja':
+			clearBodyClass();
+			body.classList.add('orange');
+			break;
+		case 'Verde':
+			clearBodyClass();
+			body.classList.add('green');
+			break;
+		case 'Gris':
+			clearBodyClass();
+			body.classList.add('gray');
+			break;
 	}
 });
